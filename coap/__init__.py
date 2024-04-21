cc7e0a99e4fff62169e3b96951c24e9ab889c4fb
sofiekamber <sofie.kamber@gmail.com>
2024-04-21 21:18:54 +0200

    change coap

--- coap/__init__.py
+++ coap/__init__.py
@@ -8,8 +8,17 @@ def attach_coap(parametric_body: smplx.SMPL, pretrained=True, device=None):
     if pretrained:
         model_type = coap_body.model_type
         gender = parametric_body.gender
-        checkpoint = f'https://github.com/markomih/COAP/blob/dev/models/coap_{model_type}_{gender}.ckpt?raw=true'
-        state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=True)
+        # TODO replace with online checkpoint
+        if 'mano' in model_type and parametric_body.is_rhand:
+            checkpoint = '/home/sofie-kamber/Projects/COAP/training_code/TRAINED_MODELS/hand_models/mano/right/ckpts/last.ckpt'
+            state_dict = torch.load(checkpoint)
+        elif 'mano' in model_type and not parametric_body.is_rhand:
+            checkpoint = '/home/sofie-kamber/Projects/COAP/training_code/TRAINED_MODELS/hand_models/mano/left/ckpts/last.ckpt'
+            state_dict = torch.load(checkpoint)
+        else:
+            checkpoint = f'https://github.com/markomih/COAP/blob/dev/models/coap_{model_type}_{gender}.ckpt?raw=true'
+            state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=True)
+
         coap_body.load_state_dict(state_dict['state_dict'])
     if device is not None:
         parametric_body = parametric_body.to(device=device)
