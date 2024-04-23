f0d7e0b7e6501f0072ac8e1038342f0bdaaf3ce6
sofiekamber <sofie.kamber@gmail.com>
2024-04-23 15:55:15 +0200

    use online checkpoint instead of local one

--- coap/__init__.py
+++ coap/__init__.py
@@ -8,17 +8,13 @@ def attach_coap(parametric_body: smplx.SMPL, pretrained=True, device=None):
     if pretrained:
         model_type = coap_body.model_type
         gender = parametric_body.gender
-        # TODO replace with online checkpoint
-        if 'mano' in model_type and parametric_body.is_rhand:
-            checkpoint = '/home/sofie-kamber/Projects/COAP/training_code/TRAINED_MODELS/hand_models/mano/right/ckpts/last.ckpt'
-            state_dict = torch.load(checkpoint)
-        elif 'mano' in model_type and not parametric_body.is_rhand:
-            checkpoint = '/home/sofie-kamber/Projects/COAP/training_code/TRAINED_MODELS/hand_models/mano/left/ckpts/last.ckpt'
-            state_dict = torch.load(checkpoint)
+        if 'mano' in model_type:
+            hand_side = 'right' if parametric_body.is_rhand else 'left'
+            checkpoint = f'https://github.com/sofiekamber/COAP-checkpoints/blob/main/coap_{model_type}_{hand_side}.ckpt?raw=true'
         else:
             checkpoint = f'https://github.com/markomih/COAP/blob/dev/models/coap_{model_type}_{gender}.ckpt?raw=true'
-            state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=True)
 
+        state_dict = torch.hub.load_state_dict_from_url(checkpoint, progress=True)
         coap_body.load_state_dict(state_dict['state_dict'])
     if device is not None:
         parametric_body = parametric_body.to(device=device)
