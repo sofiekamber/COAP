b8476516993b28705e219f7eb610c93e039a602b
rfernandes@student.ethz.ch <rfernandes@student.ethz.ch>
2024-05-24 12:55:24 +0200

    added scene_collisions.py

--- tutorials/scene_collisions.py
+++ tutorials/scene_collisions.py
@@ -19,6 +19,7 @@ from coap import attach_coap
 torch.manual_seed(0)
 np.random.seed(0)
 
+
 @torch.no_grad()
 def visualize(model=None, smpl_output=None, scene_mesh=None, query_samples=None, collision_samples=None):
     if not VISUALIZE:
@@ -55,6 +56,7 @@ def visualize(model=None, smpl_output=None, scene_mesh=None, query_samples=None,
 
     VIEWER.render_lock.release()
 
+
 def load_smpl_data(pkl_path):
     def to_tensor(x, device):
         if torch.is_tensor(x):
@@ -66,7 +68,29 @@ def load_smpl_data(pkl_path):
     with open(pkl_path, 'rb') as f:
         param = pickle.load(f)
     torch_param = {key: to_tensor(val, args.device) for key, val in param.items()}
+
+    # for visualization -- disable global rotation and translation as they do not influence self-penetration
+    for zero_key in ['global_orient', 'transl', 'left_hand_pose', 'right_hand_pose']:
+        if zero_key in torch_param:
+            torch_param[zero_key][:] = 0.0
+
+    if args.model_type == 'smpl':
+        smpl_body_pose = torch.zeros((1, 69), dtype=torch.float, device=args.device)
+        smpl_body_pose[:, :63] = torch_param['body_pose']
+        torch_param['body_pose'] = smpl_body_pose
+
+    if args.model_type == 'mano':
+        smpl_body_pose = torch.zeros((1, 48), dtype=torch.float, device=args.device)
+        smpl_body_pose[:, :48] = torch.from_numpy(torch_param['right' if torch_param['right'] else 'left']['pose']).to(
+            args.device)
+        torch_param['hand_pose'] = smpl_body_pose.to(torch.float32)
+        torch_param['betas'] = torch.from_numpy(torch_param['right' if torch_param['right'] else 'left']['shape']).to(
+            torch.float32).to(args.device)
+        torch_param['transl'] = torch.from_numpy(np.array([[-5.0101800e-003, 1.5031957e-001, 3.0754370e-002]])).to(
+            torch.float32).to(args.device)
+
     return torch_param
+
 
 @torch.no_grad()
 def sample_scene_points(model, smpl_output, scene_vertices, scene_normals=None, n_upsample=2, max_queries=10000):
@@ -85,13 +109,13 @@ def sample_scene_points(model, smpl_output, scene_vertices, scene_normals=None, 
     if not colliding_inds.any():
         return None
     points = points[colliding_inds.reshape(-1)]
-    
+
     if scene_normals is not None and points.size(0) > 0:  # sample extra points if normals are available
         norms = scene_normals[inds][colliding_inds]
 
-        offsets = 0.5*torch.normal(0.05, 0.05, size=(points.shape[0]*n_upsample, 1), device=norms.device).abs()
+        offsets = 0.5 * torch.normal(0.05, 0.05, size=(points.shape[0] * n_upsample, 1), device=norms.device).abs()
         verts, norms = points.repeat(n_upsample, 1), norms.repeat(n_upsample, 1)
-        points = torch.cat((points, (verts - offsets*norms).reshape(-1, 3)), dim=0)
+        points = torch.cat((points, (verts - offsets * norms).reshape(-1, 3)), dim=0)
 
     if points.shape[0] > max_queries:
         points = points[torch.randperm(points.size(0), device=points.device)[:max_queries]]
@@ -105,31 +129,44 @@ def main():
     scene_mesh = trimesh.load_mesh(args.scan_path)
 
     # create a SMPL body and attach COAP
-    model = smplx.create(model_path=args.bm_dir_path, model_type=args.model_type, gender=args.gender, num_pca_comps=12)
+    if (args.model_type == 'mano'):
+        key_pose = 'hand_pose'
+        '''for i in range(len(data[key_pose][0])):
+            if i % 1 == 0:
+                data[key_pose][0][i] *= -1'''
+        print(data[key_pose])
+        model = smplx.create(model_path=args.bm_dir_path, is_rhand=bool(data['right']), model_type=args.model_type,
+                             gender=args.gender,
+                             num_pca_comps=1)
+    else:
+        key_pose = 'body_pose'
+        model = smplx.create(model_path=args.bm_dir_path, model_type=args.model_type, gender=args.gender,
+                             num_pca_comps=12)
     model = attach_coap(model, pretrained=True, device=args.device)
-    
+
     scene_vertices = torch.from_numpy(scene_mesh.vertices).to(device=args.device, dtype=torch.float)
-    scene_normals = torch.from_numpy(np.asarray(scene_mesh.vertex_normals).copy()).to(device=args.device, dtype=torch.float)
+    scene_normals = torch.from_numpy(np.asarray(scene_mesh.vertex_normals).copy()).to(device=args.device,
+                                                                                      dtype=torch.float)
 
     # visualize
     smpl_output = model(**data, return_verts=True, return_full_pose=True)
-    # NOTE: make sure that smpl_output contains the valid SMPL variables (pose parameters, joints, and vertices). 
+    # NOTE: make sure that smpl_output contains the valid SMPL variables (pose parameters, joints, and vertices).
     assert model.joint_mapper is None, 'COAP requires valid SMPL joints as input'
-
+    # print(data)
     visualize(model, smpl_output, scene_mesh)
     print('waiting 5 seconds')
     time.sleep(5)
-    
+
     # create an optimizer
-    init_pose = data['body_pose'].detach().clone()
-    params_to_optimize = ['transl']#, 'global_orient'
+    init_pose = data[key_pose].detach().clone()
+    params_to_optimize = [key_pose]  # 'transl', 'global_orient'
     for param in params_to_optimize:
         data[param].requires_grad = True
     opt = torch.optim.Adam([data[param] for param in params_to_optimize], lr=args.lr)
     for step in range(args.max_iters):
         # smpl forward pass
         smpl_output = model(**data, return_verts=True, return_full_pose=True)
-        
+
         # compute self collision loss
         scene_points = sample_scene_points(model, smpl_output, scene_vertices, scene_normals)
         if scene_points is None:
@@ -137,14 +174,14 @@ def main():
             break
         selfpen_loss, _collision_mask = model.coap.collision_loss(scene_points, smpl_output, ret_collision_mask=True)
 
-        # visualization and opt step 
+        # visualization and opt step
         if VISUALIZE:
             _collision_samples = scene_points[_collision_mask]
             _non_collision_samples = scene_points[~_collision_mask]
             visualize(model, smpl_output, scene_mesh, _non_collision_samples, _collision_samples)
             print('iter ', step, ':\t', selfpen_loss, '\tWaiting 0.5s')
             time.sleep(0.5)
-        
+
         if selfpen_loss < 0.5:
             print('Converged')
             break
@@ -160,25 +197,29 @@ def main():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Tutorial on how to use COAP to avoid collisions with static geometries.')
     parser.add_argument('--device', type=str, choices=['cuda', 'cpu'], default='cuda', help='Device (cuda or cpu).')
-    
+
     # SMPL specification
     parser.add_argument('--bm_dir_path', type=str, required=True, help='Directory with SMPL bodies.')
-    parser.add_argument('--model_type', type=str, choices=['smpl', 'smplx'], default='smplx', help='SMPL-based body type.')
-    parser.add_argument('--gender', type=str, choices=['male', 'female', 'neutral'], default='neutral', help='SMPL gender.')
+    parser.add_argument('--model_type', type=str, choices=['smpl', 'smplx', 'mano'], default='smplx',
+                        help='SMPL-based body type.')
+    parser.add_argument('--gender', type=str, choices=['male', 'female', 'neutral'], default='neutral',
+                        help='SMPL gender.')
 
     # data samples
-    parser.add_argument('--scan_path', type=str, default='./samples/scene_collision/raw_kinect_scan/scan.obj', help='Raw scan location.')
-    parser.add_argument('--sample_body', type=str, default='./samples/scene_collision/sample_bodies/frame_01743.pkl', help='SMPL parameters.')
+    parser.add_argument('--scan_path', type=str, default='./samples/scene_collision/raw_kinect_scan/scan.obj',
+                        help='Raw scan location.')
+    parser.add_argument('--sample_body', type=str, default='./samples/scene_collision/sample_bodies/frame_01743.pkl',
+                        help='SMPL parameters.')
 
     # optimization related
     parser.add_argument('--max_iters', default=200, type=int, help='The maximum number of optimization steps.')
     parser.add_argument('--lr', default=0.005, type=float, help='Learning rate.')
-    
+
     args = parser.parse_args()
     VISUALIZE = True
     if VISUALIZE:
         VIEWER = pyrender.Viewer(
-            pyrender.Scene(ambient_light=[0.02, 0.02, 0.02], bg_color=[1.0, 1.0, 1.0]), 
+            pyrender.Scene(ambient_light=[0.02, 0.02, 0.02], bg_color=[1.0, 1.0, 1.0]),
             use_raymond_lighting=True, run_in_thread=True, viewport_size=(1920, 1080))
 
     main()
